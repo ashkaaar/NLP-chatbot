361d46a5556d2925637054f6292c0ca2dda80520
av1shkar <vishkardandge@gmail.com>
2024-02-10 02:20:12 +0530

    run formatter

--- ignite.py
+++ ignite.py
@@ -1,4 +1,4 @@
 from openfabric_pysdk.starter import Starter
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     Starter.ignite(debug=False, host="0.0.0.0", port=5500),
--- main.py
+++ main.py
@@ -2,24 +2,25 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import streamlit as st
 import torch
 import pandas as pd
+
 # model stuff
 model_path = "./models/finetuned-science"
 
 # gettin' the model and tokenizer
 try:
-    tokenizer = AutoTokenizer.from_pretrained('microsoft/DialoGPT-small')
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
     model = AutoModelForCausalLM.from_pretrained(model_path)
 except Exception as e:
-    st.error(f'No model or tokenizer: {e}')
+    st.error(f"No model or tokenizer: {e}")
     raise
 
 # chat history
 chat_history_ids = torch.tensor([])
 
-conversation_df = pd.DataFrame(columns=['User', 'Chatbot'])
+conversation_df = pd.DataFrame(columns=["User", "Chatbot"])
 
 
-#This following is how we can execute the chatbot with the exectutre function but may not be suitable for intereacting with the chatbot locally, that's why I have commented it out
+# This following is how we can execute the chatbot with the exectutre function but may not be suitable for intereacting with the chatbot locally, that's why I have commented it out
 
 
 # ############################################################
@@ -44,53 +45,62 @@ conversation_df = pd.DataFrame(columns=['User', 'Chatbot'])
 #         # generate response
 #         chat_history_ids = model.generate(
 #             bot_input_ids, max_length=1000,
-#             pad_token_id=tokenizer.eos_token_id,  
-#             no_repeat_ngram_size=3,       
-#             do_sample=True, 
-#             top_k=50,  
-#             top_p=0.95, 
-#             temperature=1.0  
+#             pad_token_id=tokenizer.eos_token_id,
+#             no_repeat_ngram_size=3,
+#             do_sample=True,
+#             top_k=50,
+#             top_p=0.95,
+#             temperature=1.0
 #         )
-        
+
 #         # Decode the chat response
 #         chat_response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-        
+
 #         output.append(chat_response)
 
 #     return SchemaUtil.create(SimpleText(), dict(text=output))
 
 
-#This following is the streamlit UI code that will be used to interact with the chatbot
+# This following is the streamlit UI code that will be used to interact with the chatbot
 
 # Set page title
-st.set_page_config(page_title='NLP-Chatbot')
+st.set_page_config(page_title="NLP-Chatbot")
 
 # Title
-st.title('NLP-Chatbot')
+st.title("NLP-Chatbot")
 
 # User input
-user_input = st.text_input('Enter your message here')
+user_input = st.text_input("Enter your message here")
 
 # Submit button
-if st.button('Submit'):
+if st.button("Submit"):
     # turn input into tokens
-    new_user_input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors='pt')
+    new_user_input_ids = tokenizer.encode(
+        user_input + tokenizer.eos_token, return_tensors="pt"
+    )
     # add input to chat history
-    bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if chat_history_ids.shape[0] > 0 else new_user_input_ids
+    bot_input_ids = (
+        torch.cat([chat_history_ids, new_user_input_ids], dim=-1)
+        if chat_history_ids.shape[0] > 0
+        else new_user_input_ids
+    )
 
     # generate response
     chat_history_ids = model.generate(
-        bot_input_ids, max_length=1000,
-        pad_token_id=tokenizer.eos_token_id,  
-        no_repeat_ngram_size=3,       
-        do_sample=True, 
-        top_k=50,  
-        top_p=0.95, 
-        temperature=1.0  
+        bot_input_ids,
+        max_length=1000,
+        pad_token_id=tokenizer.eos_token_id,
+        no_repeat_ngram_size=3,
+        do_sample=True,
+        top_k=50,
+        top_p=0.95,
+        temperature=1.0,
     )
-    
+
     # Decode the chat response
-    chat_response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-    
+    chat_response = tokenizer.decode(
+        chat_history_ids[:, bot_input_ids.shape[-1] :][0], skip_special_tokens=True
+    )
+
     # Display chat response
-    st.text_area('Response', chat_response, height=200)
+    st.text_area("Response", chat_response, height=200)
